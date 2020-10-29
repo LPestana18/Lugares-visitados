19d4bc34341ce0ce05a14a9676c3d140689624dd
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2020-10-29 10:07:42 -0300

    Navageção

--- app/src/main/java/br/com/lucaspestana/lugares_visitados/ListaLugaresActivity.java
+++ app/src/main/java/br/com/lucaspestana/lugares_visitados/ListaLugaresActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ListaLugaresActivity extends AppCompatActivity {
 
@@ -11,9 +15,16 @@ public class ListaLugaresActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_lugares);
+        FloatingActionButton fabAdd = findViewById(R.id.fabAddLugares);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                irLugares();
+            }
+        });
     }
 
-    public void irParaCadastroLugares() {
+    public void irLugares() {
         startActivity(new Intent(this, AddLugarActivity.class));
     }
 }
---
+++ app/src/main/java/br/com/lucaspestana/lugares_visitados/recycler/LugarViewHolder.java
@@ -0,0 +1,13 @@
+package br.com.lucaspestana.lugares_visitados.recycler;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class LugarViewHolder extends RecyclerView.ViewHolder {
+
+    public LugarViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
